759cf0d26083a0322e064c72cdf6e160278a0dd0
Clay O'Dell <clay.odell.music@gmail.com>
2025-01-19 11:25:23 -0600

    another attempt to fix db.js removed readFileSync to a ssl cert

--- db.js
+++ db.js
@@ -1,19 +1,26 @@
 "use strict";
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
-
-
+const fs = require("fs");
+const path = require("path");
 
 const db = new Client({
   connectionString: getDatabaseUri(),
-
+  ssl: {
+    rejectUnauthorized: false,
+   
+  }
 });
 
+db.on('error', err => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 db.connect()
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => {
-    console.error("Connection error:", err.stack);
+  .then(() => console.log('Connected to the database'))
+  .catch(err => {
+    console.error('Connection error:', err.stack);
     process.exit(1);
   });
 
