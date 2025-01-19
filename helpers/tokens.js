9db30b016173edee0d48c809737b1156c6fcf6ac
Clay O'Dell <clay.odell.music@gmail.com>
2025-01-19 01:03:31 -0600

    updated admin and user model SQL

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -5,7 +5,7 @@ function createToken(user) {
   const payload = {
     sub: user.userid,    
     email: user.email,
-    userType: user.usertype,
+    
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
   return token;
