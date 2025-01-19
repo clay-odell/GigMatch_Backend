1d98d8761068edc4df48d456e33be85d9f776a28
Clay O'Dell <clay.odell.music@gmail.com>
2025-01-18 20:12:01 -0600

    updated app.js

--- app.js
+++ app.js
@@ -19,14 +19,6 @@ app.use("/user", require("./routes/userRoutes"));
 app.use("/event", require("./routes/eventRequest"));
 app.use("/admin", require("./routes/adminRoutes"));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist", "assets"))); // Ensure this matches your build directory path
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); // Ensure this matches your build directory path
-  });
-}
 
 // Handle 404 errors
 app.use((req, res, next) => {
