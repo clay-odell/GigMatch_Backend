75f4aadbbfcd2aed09f39744534a0ef7e5354ca9
Clay O'Dell <clay.odell.music@gmail.com>
2025-01-19 11:52:03 -0600

    attempting to fix notfound error on / route

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const { NotFoundError } = require('./expressError');
-const userRoutes = require('./routes/userRoutes');
-const eventRoutes = require('./routes/eventRequest');
-const adminRoutes = require('./routes/adminRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { NotFoundError } = require("./expressError");
+const userRoutes = require("./routes/userRoutes");
+const eventRoutes = require("./routes/eventRequest");
+const adminRoutes = require("./routes/adminRoutes");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
 
-app.use('/user', userRoutes);
-app.use('/event', eventRoutes);
-app.use('/admin', adminRoutes);
+//Define root
+app.get("/", (req, res) => {
+  res.send("Welcome to GigMatch Server");
+});
+
+app.use("/user", userRoutes);
+app.use("/event", eventRoutes);
+app.use("/admin", adminRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 console.log(port);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the GigMatch Server');
-})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
