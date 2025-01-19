046d3c0fc6348403a8f9ea35ae920608bc0a14b4
Clay O'Dell <clay.odell.music@gmail.com>
2025-01-18 18:32:32 -0600

    updated app to have dist instead of build to be vite compatible

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { NotFoundError, BadRequestError } = require("./expressError");
-const userRoutes = require("./routes/userRoutes");
-const eventRoutes = require("./routes/eventRequest");
-const adminRoutes = require("./routes/adminRoutes");
 const path = require("path");
 
 // Initialize Express App
@@ -17,27 +14,26 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
 
-// Routes
-app.use("/user", userRoutes);
-app.use("/event", eventRoutes);
-app.use("/admin", adminRoutes);
+// API Routes
+app.use("/user", require("./routes/userRoutes"));
+app.use("/event", require("./routes/eventRequest"));
+app.use("/admin", require("./routes/adminRoutes"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
-// Handle 404 errors -- This matches everything
+// Handle 404 errors
 app.use((req, res, next) => {
   return next(new NotFoundError());
 });
 
-// Generic error handler; anything unhandled goes here
+// Generic error handler
 app.use((err, req, res, next) => {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
